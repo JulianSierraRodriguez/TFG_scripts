01cd8bc85dcb6c11c187386e3144002059d918c2
Julian S <38818144+JulianSierraRodriguez@users.noreply.github.com>
2024-06-09 18:03:53 +0200

    Update plt_a95_d91.py

--- Python_scripts/conformation_separation/plt_a95_d91.py
+++ Python_scripts/conformation_separation/plt_a95_d91.py
@@ -56,12 +56,6 @@ file2_4GNG = "data/bak_4GNG_d91.dat"
 
 ######
 
-file_10  = "data/bak_10_a95.dat"
-file2_10 = "data/bak_10_d91.dat"
-
-file_10G  = "data/bak_10G_a95.dat"
-file2_10G = "data/bak_10G_d91.dat"
-
 file_10N  = "data/bak_10N_a95.dat"
 file2_10N = "data/bak_10N_d91.dat"
 
@@ -112,12 +106,6 @@ data2_4GNG = np.loadtxt(file2_4GNG)[:, 1]
 
 ######
 
-data_10  = np.loadtxt(file_10)[:, 1]
-data2_10 = np.loadtxt(file2_10)[:, 1]
-
-data_10G  = np.loadtxt(file_10G)[:, 1]
-data2_10G = np.loadtxt(file2_10G)[:, 1]
-
 data_10N  = np.loadtxt(file_10N)[:, 1]
 data2_10N = np.loadtxt(file2_10N)[:, 1]
 
@@ -133,8 +121,6 @@ fig1, ax1 = plt.subplots(figsize=(10,7))
 ax1.scatter(data_bir[0], data2_bir[0], c='m', marker = ".", linestyle = "", label = 'bir')
 ax1.scatter(data_bir[1], data2_bir[1], c='m', marker = ".", linestyle = "")
 ax1.scatter(data_bir[2], data2_bir[2], c='m', marker = ".", linestyle = "")
-# data_bir[3] = data_bir[3] - 360
-# data2_bir[3] = data2_bir[3] - 360
 ax1.scatter(data_bir[3], data2_bir[3], c='m', marker = ".", linestyle = "")
 ax1.scatter(data_bir[4], data2_bir[4], c='m', marker = ".", linestyle = "")
 ax1.scatter(data_bir[5], data2_bir[5], c='m', marker = ".", linestyle = "")
@@ -190,7 +176,6 @@ ax1.scatter(data_17L[3], data2_17L[3], c='r', marker = "s", linestyle = "")
 ax1.scatter(data_17L[4], data2_17L[4], c='r', marker = "s", linestyle = "")
 ax1.scatter(data_17L[5], data2_17L[5], c='b', marker = "s", linestyle = "")
 ax1.scatter(data_17L[6], data2_17L[6], c='r', marker = "s", linestyle = "")
-# data_17L[7] = data_17L[7] + 360
 ax1.scatter(data_17L[7], data2_17L[7], c='r', marker = "s", linestyle = "")
 ax1.scatter(data_17L[8], data2_17L[8], c='r', marker = "s", linestyle = "")
 ax1.scatter(data_17L[9], data2_17L[9], c='r', marker = "s", linestyle = "")
@@ -207,20 +192,14 @@ ax1.scatter(data_17LG[8], data2_17LG[8], c='r', marker = "s", linestyle = "")
 ax1.scatter(data_17LG[9], data2_17LG[9], c='r', marker = "s", linestyle = "")
 
 ax1.scatter(data_17LN[0], data2_17LN[0], c='b', marker = "s", linestyle = "")
-# data_17LN[1] = data_17LN[1] + 360
 ax1.scatter(data_17LN[1], data2_17LN[1], c='r', marker = "s", linestyle = "")
-# data_17LN[2] = data_17LN[2] + 360
 ax1.scatter(data_17LN[2], data2_17LN[2], c='r', marker = "s", linestyle = "")
 ax1.scatter(data_17LN[3], data2_17LN[3], c='b', marker = "s", linestyle = "")
 ax1.scatter(data_17LN[4], data2_17LN[4], c='r', marker = "s", linestyle = "")
-# data_17LN[5] = data_17LN[5] + 360
 ax1.scatter(data_17LN[5], data2_17LN[5], c='b', marker = "s", linestyle = "")
-# data_17LN[6] = data_17LN[6] + 360
 ax1.scatter(data_17LN[6], data2_17LN[6], c='b', marker = "s", linestyle = "")
 ax1.scatter(data_17LN[7], data2_17LN[7], c='b', marker = "s", linestyle = "")
-# data_17LN[8] = data_17LN[8] + 360
 ax1.scatter(data_17LN[8], data2_17LN[8], c='b', marker = "s", linestyle = "")
-# data_17LN[9] = data_17LN[9] + 360
 ax1.scatter(data_17LN[9], data2_17LN[9], c='r', marker = "s", linestyle = "")
 
 ax1.scatter(data_17LNG[0], data2_17LNG[0], c='b', marker = "s", linestyle = "")
@@ -245,29 +224,27 @@ ax1.scatter(data_4G[4], data2_4G[4], c='c', marker = "^", linestyle = "")
 ax1.scatter(data_4G[5], data2_4G[5], c='c', marker = "^", linestyle = "")
 ax1.scatter(data_4G[6], data2_4G[6], c='c', marker = "^", linestyle = "")
 ax1.scatter(data_4G[7], data2_4G[7], c='c', marker = "^", linestyle = "")
-# data2_4G[8] = data2_4G[8] - 360
 ax1.scatter(data_4G[8], data2_4G[8], c='c', marker = "^", linestyle = "")
 ax1.scatter(data_4G[9], data2_4G[9], c='c', marker = "^", linestyle = "")
 
-# ax1.scatter(data_4GG[0], data2_4GG[0], c='mediumspringgreen', marker = "^", linestyle = "")
-# ax1.scatter(data_4GG[1], data2_4GG[1], c='mediumspringgreen', marker = "^", linestyle = "")
-# data_4GG[2] = data_4GG[2] + 360
-# ax1.scatter(data_4GG[2], data2_4GG[2], c='mediumspringgreen', marker = "^", linestyle = "")
-# data_4GG[3] = data_4GG[3] + 360
-# ax1.scatter(data_4GG[3], data2_4GG[3], c='mediumspringgreen', marker = "^", linestyle = "")
-# data_4GG[4] = data_4GG[4] + 360
-# ax1.scatter(data_4GG[4], data2_4GG[4], c='mediumspringgreen', marker = "^", linestyle = "")
-# data_4GG[5] = data_4GG[5] + 360
-# ax1.scatter(data_4GG[5], data2_4GG[5], c='mediumspringgreen', marker = "^", linestyle = "")
-# ax1.scatter(data_4GG[6], data2_4GG[6], c='mediumspringgreen', marker = "^", linestyle = "")
-# ax1.scatter(data_4GG[7], data2_4GG[7], c='mediumspringgreen', marker = "^", linestyle = "")
-# ax1.scatter(data_4GG[8], data2_4GG[8], c='mediumspringgreen', marker = "^", linestyle = "")
-# data_4GG[9] = data_4GG[9] + 360
-# ax1.scatter(data_4GG[9], data2_4GG[9], c='mediumspringgreen', marker = "^", linestyle = "")
+ax1.scatter(data_4GG[0], data2_4GG[0], c='mediumspringgreen', marker = "^", linestyle = "")
+ax1.scatter(data_4GG[1], data2_4GG[1], c='mediumspringgreen', marker = "^", linestyle = "")
+data_4GG[2] = data_4GG[2] + 360
+ax1.scatter(data_4GG[2], data2_4GG[2], c='mediumspringgreen', marker = "^", linestyle = "")
+data_4GG[3] = data_4GG[3] + 360
+ax1.scatter(data_4GG[3], data2_4GG[3], c='mediumspringgreen', marker = "^", linestyle = "")
+data_4GG[4] = data_4GG[4] + 360
+ax1.scatter(data_4GG[4], data2_4GG[4], c='mediumspringgreen', marker = "^", linestyle = "")
+data_4GG[5] = data_4GG[5] + 360
+ax1.scatter(data_4GG[5], data2_4GG[5], c='mediumspringgreen', marker = "^", linestyle = "")
+ax1.scatter(data_4GG[6], data2_4GG[6], c='mediumspringgreen', marker = "^", linestyle = "")
+ax1.scatter(data_4GG[7], data2_4GG[7], c='mediumspringgreen', marker = "^", linestyle = "")
+ax1.scatter(data_4GG[8], data2_4GG[8], c='mediumspringgreen', marker = "^", linestyle = "")
+data_4GG[9] = data_4GG[9] + 360
+ax1.scatter(data_4GG[9], data2_4GG[9], c='mediumspringgreen', marker = "^", linestyle = "")
 
 ax1.scatter(data_4GN[0], data2_4GN[0], c='c', marker = "^", linestyle = "")
 ax1.scatter(data_4GN[1], data2_4GN[1], c='c', marker = "^", linestyle = "")
-# data2_4GN[2] = data2_4GN[2] - 360
 ax1.scatter(data_4GN[2], data2_4GN[2], c='c', marker = "^", linestyle = "")
 ax1.scatter(data_4GN[3], data2_4GN[3], c='c', marker = "^", linestyle = "")
 ax1.scatter(data_4GN[4], data2_4GN[4], c='c', marker = "^", linestyle = "")
@@ -283,7 +260,6 @@ ax1.scatter(data_4GNG[2], data2_4GNG[2], c='c', marker = "^", linestyle = "")
 ax1.scatter(data_4GNG[3], data2_4GNG[3], c='c', marker = "^", linestyle = "")
 ax1.scatter(data_4GNG[4], data2_4GNG[4], c='c', marker = "^", linestyle = "")
 ax1.scatter(data_4GNG[5], data2_4GNG[5], c='c', marker = "^", linestyle = "")
-# data_4GNG[6] = data_4GNG[6] - 360
 ax1.scatter(data_4GNG[6], data2_4GNG[6], c='c', marker = "^", linestyle = "")
 ax1.scatter(data_4GNG[7], data2_4GNG[7], c='c', marker = "^", linestyle = "")
 ax1.scatter(data_4GNG[8], data2_4GNG[8], c='c', marker = "^", linestyle = "")
@@ -291,56 +267,30 @@ ax1.scatter(data_4GNG[9], data2_4GNG[9], c='c', marker = "^", linestyle = "")
 
 ####################################################
 
-# ax1.scatter(data_10[0], data2_10[0], c='y', marker = "*", linestyle = "", label = '10')
-# ax1.scatter(data_10[1], data2_10[1], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10[2], data2_10[2], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10[3], data2_10[3], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10[4], data2_10[4], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10[5], data2_10[5], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10[6], data2_10[6], c='y', marker = "*", linestyle = "")
-# data_10[7] = data_10[7] + 360
-# ax1.scatter(data_10[7], data2_10[7], c='y', marker = "*", linestyle = "")
-# data_10[8] = data_10[8] + 360
-# ax1.scatter(data_10[8], data2_10[8], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10[9], data2_10[9], c='y', marker = "*", linestyle = "")
+ax1.scatter(data_10N[0], data2_10N[0], c='y', marker = "*", linestyle = "")
+ax1.scatter(data_10N[1], data2_10N[1], c='y', marker = "*", linestyle = "")
+ax1.scatter(data_10N[2], data2_10N[2], c='y', marker = "*", linestyle = "")
+ax1.scatter(data_10N[3], data2_10N[3], c='y', marker = "*", linestyle = "")
+ax1.scatter(data_10N[4], data2_10N[4], c='y', marker = "*", linestyle = "")
+ax1.scatter(data_10N[5], data2_10N[5], c='y', marker = "*", linestyle = "")
+data_10N[6] = data_10N[6] + 360
+ax1.scatter(data_10N[6], data2_10N[6], c='y', marker = "*", linestyle = "")
+ax1.scatter(data_10N[7], data2_10N[7], c='y', marker = "*", linestyle = "")
+ax1.scatter(data_10N[8], data2_10N[8], c='y', marker = "*", linestyle = "")
+ax1.scatter(data_10N[9], data2_10N[9], c='y', marker = "*", linestyle = "")
 
-# ax1.scatter(data_10G[0], data2_10G[0], c='orange', marker = "*", linestyle = "")
-# ax1.scatter(data_10G[1], data2_10G[1], c='orange', marker = "*", linestyle = "")
-# ax1.scatter(data_10G[2], data2_10G[2], c='orange', marker = "*", linestyle = "")
-# data_10G[3] = data_10G[3] + 360
-# ax1.scatter(data_10G[3], data2_10G[3], c='orange', marker = "*", linestyle = "")
-# ax1.scatter(data_10G[4], data2_10G[4], c='orange', marker = "*", linestyle = "")
-# data_10G[5] = data_10G[5] + 360
-# ax1.scatter(data_10G[5], data2_10G[5], c='orange', marker = "*", linestyle = "")
-# ax1.scatter(data_10G[6], data2_10G[6], c='orange', marker = "*", linestyle = "")
-# ax1.scatter(data_10G[7], data2_10G[7], c='orange', marker = "*", linestyle = "")
-# ax1.scatter(data_10G[8], data2_10G[8], c='orange', marker = "*", linestyle = "")
-# ax1.scatter(data_10G[9], data2_10G[9], c='orange', marker = "*", linestyle = "")
-
-# ax1.scatter(data_10N[0], data2_10N[0], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10N[1], data2_10N[1], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10N[2], data2_10N[2], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10N[3], data2_10N[3], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10N[4], data2_10N[4], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10N[5], data2_10N[5], c='y', marker = "*", linestyle = "")
-# data_10N[6] = data_10N[6] + 360
-# ax1.scatter(data_10N[6], data2_10N[6], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10N[7], data2_10N[7], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10N[8], data2_10N[8], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10N[9], data2_10N[9], c='y', marker = "*", linestyle = "")
-
-# ax1.scatter(data_10NG[0], data2_10NG[0], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10NG[1], data2_10NG[1], c='orange', marker = "*", linestyle = "")
-# ax1.scatter(data_10NG[2], data2_10NG[2], c='y', marker = "*", linestyle = "")
-# data_10NG[3] = data_10NG[3] + 360
-# ax1.scatter(data_10NG[3], data2_10NG[3], c='orange', marker = "*", linestyle = "")
-# ax1.scatter(data_10NG[4], data2_10NG[4], c='orange', marker = "*", linestyle = "")
-# data_10NG[5] = data_10NG[5] + 360
-# ax1.scatter(data_10NG[5], data2_10NG[5], c='orange', marker = "*", linestyle = "")
-# ax1.scatter(data_10NG[6], data2_10NG[6], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10NG[7], data2_10NG[7], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10NG[8], data2_10NG[8], c='y', marker = "*", linestyle = "")
-# ax1.scatter(data_10NG[9], data2_10NG[9], c='y', marker = "*", linestyle = "")
+ax1.scatter(data_10NG[0], data2_10NG[0], c='y', marker = "*", linestyle = "")
+ax1.scatter(data_10NG[1], data2_10NG[1], c='orange', marker = "*", linestyle = "")
+ax1.scatter(data_10NG[2], data2_10NG[2], c='y', marker = "*", linestyle = "")
+data_10NG[3] = data_10NG[3] + 360
+ax1.scatter(data_10NG[3], data2_10NG[3], c='orange', marker = "*", linestyle = "")
+ax1.scatter(data_10NG[4], data2_10NG[4], c='orange', marker = "*", linestyle = "")
+data_10NG[5] = data_10NG[5] + 360
+ax1.scatter(data_10NG[5], data2_10NG[5], c='orange', marker = "*", linestyle = "")
+ax1.scatter(data_10NG[6], data2_10NG[6], c='y', marker = "*", linestyle = "")
+ax1.scatter(data_10NG[7], data2_10NG[7], c='y', marker = "*", linestyle = "")
+ax1.scatter(data_10NG[8], data2_10NG[8], c='y', marker = "*", linestyle = "")
+ax1.scatter(data_10NG[9], data2_10NG[9], c='y', marker = "*", linestyle = "")
 
 ####################################################
 
@@ -402,11 +352,11 @@ def label():
           ax1.annotate("c"+l, (data_4G[i]+0.1, data2_4G[i]+0.1), size = size_label, 
                   bbox=dict(boxstyle='sawtooth,pad=0.3', fc='navajowhite', ec='black'))
  
- # for i in range(0,10,1):
- #          l = str(i)
- #          i = int(i)
- #          ax1.annotate("c"+l, (data_4GG[i]+0.1, data2_4GG[i]+0.1), size = size_label, 
- #                  bbox=dict(boxstyle='sawtooth,pad=0.3', fc='lightblue', ec='black'))
+ for i in range(0,10,1):
+         l = str(i)
+	 i = int(i)
+         ax1.annotate("c"+l, (data_4GG[i]+0.1, data2_4GG[i]+0.1), size = size_label, 
+                  bbox=dict(boxstyle='sawtooth,pad=0.3', fc='lightblue', ec='black'))
 
  for i in range(0,10,1):
           l = str(i)
@@ -422,29 +372,17 @@ def label():
 
 #####
 
- # for i in range(0,10,1):
- #          l = str(i)
- #          i = int(i)
- #          ax1.annotate("c"+l, (data_10[i]+0.1, data2_10[i]+0.1), size = size_label,
- #                  bbox=dict(boxstyle='round,pad=0.3', fc='navajowhite', ec='black'))
+for i in range(0,10,1):
+        l = str(i)
+	i = int(i)
+	ax1.annotate("c"+l, (data_10N[i]+0.1, data2_10N[i]+0.1), size = size_label,
+		     bbox=dict(boxstyle='round,pad=0.3', fc='lightgreen', ec='black'))
 
- # for i in range(0,10,1):
- #          l = str(i)
- #          i = int(i)
- #          ax1.annotate("c"+l, (data_10G[i]+0.1, data2_10G[i]+0.1), size = size_label,
- #                  bbox=dict(boxstyle='round,pad=0.3', fc='lightblue', ec='black'))
-
- # for i in range(0,10,1):
- #          l = str(i)
- #          i = int(i)
- #          ax1.annotate("c"+l, (data_10N[i]+0.1, data2_10N[i]+0.1), size = size_label,
- #                  bbox=dict(boxstyle='round,pad=0.3', fc='lightgreen', ec='black'))
-
- # for i in range(0,10,1):
- #         l = str(i)
- #         i = int(i)
- #         ax1.annotate("c"+l, (data_10NG[i]+0.1, data2_10NG[i]+0.1), size = size_label,
- #                 bbox=dict(boxstyle='round,pad=0.3', fc='pink', ec='black'))
+ for i in range(0,10,1):
+         l = str(i)
+	 i = int(i)
+	 ax1.annotate("c"+l, (data_10NG[i]+0.1, data2_10NG[i]+0.1), size = size_label,
+		      bbox=dict(boxstyle='round,pad=0.3', fc='pink', ec='black'))
         
 #####
 
@@ -467,15 +405,11 @@ if labels:
 else:
     plt.legend()
 
-# ax1.hlines(200, 260, 450)
-# ax1.hlines(297, 325, 450)
+
 
 ax1.vlines(0, 40, 75)
 ax1.vlines(360, 40, 75)
-# ax1.vlines(194, 53.25, 85)
-# ax1.vlines(150, 53.25, 85)
 
-# ax1.set_xlim([250,300])
 
 plt.xlabel(r'distance $\alpha$6 and $\alpha$1')
 plt.ylabel(r'distance $\alpha$9 and $\alpha$6')
