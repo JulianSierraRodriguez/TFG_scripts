f1deff0edb8d0ea6e3adc44c14127098b29c6911
Julian S <38818144+JulianSierraRodriguez@users.noreply.github.com>
2024-06-09 18:07:01 +0200

    Update plt_a95_mem_clust.py

--- Python_scripts/membrane_angle/clusters/plt_a95_mem_clust.py
+++ Python_scripts/membrane_angle/clusters/plt_a95_mem_clust.py
@@ -64,12 +64,6 @@ file2_10 = "data/bak_10_a_mem_a9.dat"
 file_10G  = "data/bak_10G_a95.dat"
 file2_10G = "data/bak_10G_a_mem_a9.dat"
 
-file_10N  = "data/bak_10N_a95.dat"
-file2_10N = "data/bak_10N_a_mem_a9.dat"
-
-file_10NG  = "data/bak_10NG_a95.dat"
-file2_10NG = "data/bak_10NG_a_mem_a9.dat"
-
 #####
 
 data_bir  = np.loadtxt(file_bir)[:, 1]
@@ -120,12 +114,6 @@ data2_10 = np.loadtxt(file2_10)[:, 1]
 data_10G  = np.loadtxt(file_10G)[:, 1]
 data2_10G = np.loadtxt(file2_10G)[:, 1]
 
-data_10N  = np.loadtxt(file_10N)[:, 1]
-data2_10N = np.loadtxt(file2_10N)[:, 1]
-
-data_10NG  = np.loadtxt(file_10NG)[:, 1]
-data2_10NG = np.loadtxt(file2_10NG)[:, 1]
-
 #####
 
 def arreglador_de_puntos(data):
@@ -133,24 +121,14 @@ def arreglador_de_puntos(data):
 		J = int(j)
 		if data[J] >100 :
 			data[J] = 180 - data[J]
-#	return data
-
-
-#####
-
-# fig1, ax1 = plt.subplots(figsize=(10,7))
-# ax1.get_xaxis().set_visible(False)
 
 
 ########################################################
-#%%
+
 x = 0
 data_bir = np.concatenate((data2_bir, data2_birG, data2_birN, data2_birNG), axis = None)
+
 arreglador_de_puntos(data_bir)
-# ax1.scatter(np.zeros_like(data_bir)+x,data_bir)
-#ax1.errorbar(np.zeros_like(data_bir)+x,data_bir, color='black')
-#ax1.boxplot(data_bir, whis=99, positions=[0,1])
-#plt.setp(box['data_bir'][0], color='black')
 
 ##################################################
 
@@ -159,15 +137,11 @@ data_17L = np.concatenate((data2_17L, data2_17LG, data2_17LN, data2_17LNG), axis
 
 arreglador_de_puntos(data_17L)
 
-# ax1.scatter(np.zeros_like(data_17L)+x,data_17L)
-
 ####################################################
 x = 2
 data_4G = np.concatenate((data2_4G, data2_4GG, data2_4GN, data2_4GNG), axis = None)
 
 arreglador_de_puntos(data_4G)
-
-# ax1.scatter(np.zeros_like(data_4G)+x,data_4G)
 
 ####################################################
 x = 3
@@ -176,20 +150,12 @@ data_10 = np.concatenate(( data2_10N, data2_10NG), axis = None)
 
 arreglador_de_puntos(data_10)
 
-
-# ax1.text(0-0.02,-4, 'bir', bbox=dict(facecolor='w', edgecolor = 'b'))
-# ax1.text(1-0.02,-4, '17L', bbox=dict(facecolor='w', edgecolor = 'orange'))
-# ax1.text(2-0.02,-4, '4G', bbox=dict(facecolor='w', edgecolor = 'g'))
-# ax1.text(3-0.02,-4, '10', bbox=dict(facecolor='w', edgecolor = 'r'))
-
 ####################################################
-#%%
 
 fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2)
 ax1.hist(data_bir, bins=400)
 ax1.set_title('Bir')
 ax1.set_xlabel('Protein angle of insertion')
-# print(statistics.mean(data_bir), np.median(data_bir), st.iqr(data_bir))
 ax2.hist(data_17L, bins=400)
 ax2.set_title('17L')
 ax2.set_xlabel('Protein angle of insertion')
@@ -205,7 +171,8 @@ plt.savefig('Complete_angle_histograms.png', dpi=600)
 plt.show()
 
 plt.close()
-#%%
+
+################
 
 print('BIR:')
 print('median:   '+str(np.median(data_bir)))
